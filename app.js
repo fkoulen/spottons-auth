f7b65ff5d118f7f8536c2132edcc1cf30b561c69
F.S. Koulen <feryllia.koulen@hva.nl>
2021-05-02 20:40:15 +0200

    Add route for requesting auth

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/',(req,res)=>{
-    res.send('Hello World!')
-})
 
-app.listen(port,()=>{
-    console.log(`App listening at http://localhost:${port}`)
+app.get('/auth', function (req, res) {
+    // Scopes must be separated by spaces
+    const scopes = 'user-modify-playback-state'
+
+    const redirect_uri = 'http://localhost:3000/auth/spotify/callback'
+
+    res.redirect(`https://accounts.spotify.com/authorize` +
+        `?client_id=${process.env.CLIENT_ID}` +
+        `&response_type=code` +
+        `&redirect_uri=${encodeURIComponent(redirect_uri)}` +
+        `&scope=${encodeURIComponent(scopes)}` +
+        `&show_dialog=true`)
+});
+
+
+app.listen(port, () => {
+    console.log(`You can now access http://localhost:${port}/auth`)
 })
